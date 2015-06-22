f5af9883950e4be23d405db2f88169c040b9a15e
Chris Mitchell <chris.mit7@gmail.com>
2015-06-22 15:30:56 -0400

    setup.py update

--- setup.py
+++ setup.py
@@ -6,21 +6,19 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='clinto',
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(),
     scripts=[],
     install_requires = ['six',],
     include_package_data=True,
     description='Clinto',
-    url='http://www.github.com/chris7/clinto',
+    url='http://www.github.com/wooey/clinto',
     author='Chris Mitchell',
     author_email='chris.mit7@gmail.com',
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 2.7',
     ],
 )
 
