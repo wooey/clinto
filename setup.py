ea24881b146ea04e12580583887899ae72201c09
Chris Mitchell <chris.mit7@gmail.com>
2019-02-19 22:23:56 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='clinto',
-    version='0.2.1',
+    version='0.3.0',
     packages=find_packages(),
     scripts=[],
     install_requires = ['six',],
