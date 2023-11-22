9baa175c3e1cc1c25fca3f166b9cf99c8d2eb565
Chris Mitchell <chris@ginkgobioworks.com>
2023-11-22 17:16:17 -0500

    Use astor for codegen and drop six

--- clinto/parsers/docopt_.py
+++ clinto/parsers/docopt_.py
@@ -7,12 +7,16 @@ import imp
 import inspect
 import tempfile
 import traceback
-import six
 from collections import OrderedDict
 from itertools import chain
 from ..ast import source_parser
 from ..utils import is_upload, expand_iterable
-from .base import BaseParser, parse_args_monkeypatch, ClintoArgumentParserException, update_dict_copy
+from .base import (
+    BaseParser,
+    parse_args_monkeypatch,
+    ClintoArgumentParserException,
+    update_dict_copy,
+)
 
 try:
     import docopt
@@ -20,63 +24,91 @@ except ImportError:
     docopt = None
 
 
-GLOBAL_ATTRS = ['model', 'type']
+GLOBAL_ATTRS = ["model", "type"]
 
 
 GLOBAL_ATTR_KWARGS = {
-    'name': {'action_name': 'dest'},
-    'value': {'action_name': 'default'},
-    'required': {'action_name': 'required'},
-    'help': {'action_name': 'help'},
-    }
+    "name": {"action_name": "dest"},
+    "value": {"action_name": "default"},
+    "required": {"action_name": "required"},
+    "help": {"action_name": "help"},
+}
 
 TYPE_FIELDS = {
     # Python Builtins
-    bool: {'model': 'BooleanField', 'type': 'checkbox', 'nullcheck': lambda x: x.value is None,
-           'attr_kwargs': GLOBAL_ATTR_KWARGS},
-    float: {'model': 'FloatField', 'type': 'text', 'html5-type': 'number', 'nullcheck': lambda x: x.value is None,
-            'attr_kwargs': GLOBAL_ATTR_KWARGS},
-    int: {'model': 'IntegerField', 'type': 'text', 'nullcheck': lambda x: x.value is None,
-          'attr_kwargs': GLOBAL_ATTR_KWARGS},
-    None: {'model': 'CharField', 'type': 'text', 'nullcheck': lambda x: x.value is None,
-          'attr_kwargs': GLOBAL_ATTR_KWARGS},
-    str: {'model': 'CharField', 'type': 'text', 'nullcheck': lambda x: x.value == '' or x.value is None,
-          'attr_kwargs': GLOBAL_ATTR_KWARGS},
+    bool: {
+        "model": "BooleanField",
+        "type": "checkbox",
+        "nullcheck": lambda x: x.value is None,
+        "attr_kwargs": GLOBAL_ATTR_KWARGS,
+    },
+    float: {
+        "model": "FloatField",
+        "type": "text",
+        "html5-type": "number",
+        "nullcheck": lambda x: x.value is None,
+        "attr_kwargs": GLOBAL_ATTR_KWARGS,
+    },
+    int: {
+        "model": "IntegerField",
+        "type": "text",
+        "nullcheck": lambda x: x.value is None,
+        "attr_kwargs": GLOBAL_ATTR_KWARGS,
+    },
+    None: {
+        "model": "CharField",
+        "type": "text",
+        "nullcheck": lambda x: x.value is None,
+        "attr_kwargs": GLOBAL_ATTR_KWARGS,
+    },
+    str: {
+        "model": "CharField",
+        "type": "text",
+        "nullcheck": lambda x: x.value == "" or x.value is None,
+        "attr_kwargs": GLOBAL_ATTR_KWARGS,
+    },
 }
-
 
 
 class DocOptNode(object):
     """
-        This class takes an argument parser entry and assigns it to a Build spec
+    This class takes an argument parser entry and assigns it to a Build spec
     """
+
     def __init__(self, name, option=None):
         field_type = TYPE_FIELDS.get(option.type)
         if field_type is None:
-            field_types = [i for i in fields.keys() if i is not None and issubclass(type(option.type), i)]
+            field_types = [
+                i
+                for i in fields.keys()
+                if i is not None and issubclass(type(option.type), i)
+            ]
             if len(field_types) > 1:
-                field_types = [i for i in fields.keys() if i is not None and isinstance(option.type, i)]
+                field_types = [
+                    i
+                    for i in fields.keys()
+                    if i is not None and isinstance(option.type, i)
+                ]
             if len(field_types) == 1:
                 field_type = fields[field_types[0]]
         self.node_attrs = dict([(i, field_type[i]) for i in GLOBAL_ATTRS])
-        self.node_attrs['name'] = name
-        self.node_attrs['param'] = option.long if option.long else option.short
+        self.node_attrs["name"] = name
+        self.node_attrs["param"] = option.long if option.long else option.short
 
     @property
     def name(self):
-        return self.node_attrs.get('name')
+        return self.node_attrs.get("name")
 
     def __str__(self):
         return json.dumps(self.node_attrs)
 
 
 class DocOptParser(BaseParser):
-
     def heuristic(self):
         return [
-            self.script_ext in ['.py', '.py3', '.py2'],
-            'docopt' in self.script_source,
-            '__doc__' in self.script_source,
+            self.script_ext in [".py", ".py3", ".py2"],
+            "docopt" in self.script_source,
+            "__doc__" in self.script_source,
         ]
 
     def extract_parser(self):
@@ -84,12 +116,12 @@ class DocOptParser(BaseParser):
             return False
 
         try:
-            module = imp.new_module('__name__')
-            six.exec_(self.script_source, module.__dict__)
+            module = imp.new_module("__name__")
+            exec(self.script_source, module.__dict__)
 
         except Exception:
-            sys.stderr.write('Error while loading {0}:\n'.format(self.script_path))
-            self.error = '{0}\n'.format(traceback.format_exc())
+            sys.stderr.write("Error while loading {0}:\n".format(self.script_path))
+            self.error = "{0}\n".format(traceback.format_exc())
             sys.stderr.write(self.error)
             return
 
@@ -117,14 +149,14 @@ class DocOptParser(BaseParser):
 
         try:
             # Parse with --help to enforce exit
-            usage_sections = docopt.docopt(self.parser, ['--help'])
+            usage_sections = docopt.docopt(self.parser, ["--help"])
         except SystemExit as e:
             parser = inspect.trace()[-2][0].f_locals
 
-        '''
+        """
         docopt represents all values as strings and doesn't automatically cast, we probably want to do
         some testing to see if we can convert the default value (Option.value) to a particular type.
-        '''
+        """
 
         def guess_type(s):
             try:
@@ -136,21 +168,21 @@ class DocOptParser(BaseParser):
 
             return type(v)
 
-        self.script_groups = ['Arguments']
+        self.script_groups = ["Arguments"]
         self.nodes = OrderedDict()
         self.containers = OrderedDict()
-        self.containers['default'] = []
+        self.containers["default"] = []
 
-        for option in parser['options']:
-            if option.long in ['--help', '--version']:
+        for option in parser["options"]:
+            if option.long in ["--help", "--version"]:
                 continue
 
             option.type = guess_type(option.value)
-            option_name = option.long.strip('-')
+            option_name = option.long.strip("-")
             node = DocOptNode(option_name, option=option)
 
             self.nodes[option_name] = node
-            self.containers['default'].append(option_name)
+            self.containers["default"].append(option_name)
 
         self.class_name = os.path.splitext(os.path.basename(self.script_path))[0]
         self.script_path = self.script_path
@@ -161,17 +193,17 @@ class DocOptParser(BaseParser):
     def get_script_description(self):
         # There are no real subparsers in docopt that we can access via introspection so we use the default
         # subparser of '' (no subparser/main parser)
-        parser_name = ''
-        parser_inputs = [{
-            'group': container_name,
-            'nodes': [self.nodes[node].node_attrs for node in nodes]
-            } for container_name, nodes in six.iteritems(self.containers)
+        parser_name = ""
+        parser_inputs = [
+            {
+                "group": container_name,
+                "nodes": [self.nodes[node].node_attrs for node in nodes],
+            }
+            for container_name, nodes in self.containers.items()
         ]
         return {
-            'name': self.class_name,
-            'path': self.script_path,
-            'description': self.script_description,
-            'inputs': OrderedDict([(parser_name, parser_inputs)])
+            "name": self.class_name,
+            "path": self.script_path,
+            "description": self.script_description,
+            "inputs": OrderedDict([(parser_name, parser_inputs)]),
         }
-
-
--- clinto/tests/factories.py
+++ clinto/tests/factories.py
@@ -1,38 +1,46 @@
 import argparse
-import six
-try:
-    import xrange
-except ImportError:
-    xrange = six.moves.range
 
 param_int = 0
+
 
 class BaseDict(dict):
     def __init__(self, **kwargs):
         super(BaseDict, self).__init__()
         global param_int
         base_dict = {
-            'param': '--test-param{}'.format(param_int),
-            'required': False,
+            "param": "--test-param{}".format(param_int),
+            "required": False,
         }
-        param_int = param_int+1
+        param_int = param_int + 1
         base_dict.update(**kwargs)
         self.update(**base_dict)
 
+
 class ArgParseFactory(object):
-    DESCRIPTION = 'ArgParse Factory'
-    COMMON = ['param', 'required']
+    DESCRIPTION = "ArgParse Factory"
+    COMMON = ["param", "required"]
     BASEFIELD = BaseDict()
-    FILEFIELD = BaseDict(param='--ff-out', type=argparse.FileType('w'), help='Test help')
-    UPLOADFILEFIELD = BaseDict(type=argparse.FileType('r'))
-    CHOICEFIELD = BaseDict(choices=['a', 'b', 'c'])
-    RANGECHOICES = BaseDict(choices=xrange(-10,20,2))
+    FILEFIELD = BaseDict(
+        param="--ff-out", type=argparse.FileType("w"), help="Test help"
+    )
+    UPLOADFILEFIELD = BaseDict(type=argparse.FileType("r"))
+    CHOICEFIELD = BaseDict(choices=["a", "b", "c"])
+    RANGECHOICES = BaseDict(choices=range(-10, 20, 2))
 
     def __init__(self):
         super(ArgParseFactory, self).__init__()
         self.parser = argparse.ArgumentParser(description=self.DESCRIPTION)
-        self.filefield = self.parser.add_argument(self.FILEFIELD['param'], help=self.FILEFIELD['help'],
-                                                  type=self.FILEFIELD['type'])
-        self.uploadfield = self.parser.add_argument(self.UPLOADFILEFIELD['param'], type=self.UPLOADFILEFIELD['type'])
-        self.choicefield = self.parser.add_argument(self.CHOICEFIELD['param'], choices=self.CHOICEFIELD['choices'])
-        self.rangefield = self.parser.add_argument(self.RANGECHOICES['param'], choices=self.RANGECHOICES['choices'])
+        self.filefield = self.parser.add_argument(
+            self.FILEFIELD["param"],
+            help=self.FILEFIELD["help"],
+            type=self.FILEFIELD["type"],
+        )
+        self.uploadfield = self.parser.add_argument(
+            self.UPLOADFILEFIELD["param"], type=self.UPLOADFILEFIELD["type"]
+        )
+        self.choicefield = self.parser.add_argument(
+            self.CHOICEFIELD["param"], choices=self.CHOICEFIELD["choices"]
+        )
+        self.rangefield = self.parser.add_argument(
+            self.RANGECHOICES["param"], choices=self.RANGECHOICES["choices"]
+        )
